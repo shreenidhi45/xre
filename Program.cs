13b14aad2d8f83065296916601db70b8fd4da71e
shreenidhi45 <75909459+shreenidhi45@users.noreply.github.com>
2023-09-09 19:36:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                // Write XML file
+                StreamWriter xmlWriter = new StreamWriter("C:\\Users\\shreenidhi\\Documents\\FileHandle//sampe.xml");
+                xmlWriter.WriteLine("<root>");
+                xmlWriter.WriteLine("   <element>Hello Shree</element>");
+                xmlWriter.WriteLine("</root>");
+                xmlWriter.Close();
+
+                // Write HTML file
+                StreamWriter htmlWriter = new StreamWriter("C:\\Users\\shreenidhi\\Documents\\FileHandle\\simple.html");
+                htmlWriter.WriteLine("<html>");
+                htmlWriter.WriteLine("   <body>");
+                htmlWriter.WriteLine("      <h1>Chandrayan-3 landed successfully</h1>");
+                htmlWriter.WriteLine("   </body>");
+                htmlWriter.WriteLine("</html>");
+                htmlWriter.Close();
+
+                // Write text file
+                StreamWriter textWriter = new StreamWriter("C:\\Users\\shreenidhi\\Documents\\FileHandle\\text.txt");
+                textWriter.WriteLine("This is a normal file.");
+                textWriter.Close();
+            }
+        }
+    }
+
+    
